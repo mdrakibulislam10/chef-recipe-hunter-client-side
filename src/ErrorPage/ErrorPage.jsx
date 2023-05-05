1e8cf63fee7a46b4f116fb733d4cdecab459acb6
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-05-05 16:45:48 +0600

    404 img added in err page

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,6 @@
-
 import { useNavigate, useRouteError } from "react-router-dom";
+import ErrImg from "../../public/img/err404.jpg";
+import "./ErrorPage.css";
 
 const ErrorPage = () => {
     const error = useRouteError();
@@ -9,12 +10,11 @@ const ErrorPage = () => {
 
     return (
         <div id="error-page" className="text-center mt-5">
-            <h1>404</h1>
-            <p>Page Not Found!</p>
+            <img className="err-img rounded mb-4" src={ErrImg} alt="" />
             <div>
                 <p className="fw-bold">Search Valid URL</p>
-                <p>or</p>
-                <button onClick={() => navigate(-1)} className="btn btn-warning fw-bold">Go To Previous Page</button>
+                <p>or <br /> Back To</p>
+                <button onClick={() => navigate(-1)} className="btn btn-warning fw-bold">&larr; Previous Page</button>
                 {/* <i>{error.statusText || error.message}</i> */}
             </div>
         </div>
